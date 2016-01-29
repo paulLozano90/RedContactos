3741d4bbe4bb56e3a70f4c9a26fd4ea44e1acf84
paulLozano90 <plc90@hotmail.es>
2016-01-29 14:30:04 +0100

    Servicios IOS y Win

---
+++ RedContactos/RedContactos.WinPhone/Servicios/ServicioFicheros.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+using RedContactos.Service;
+using RedContactos.WinPhone.Servicios;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ServicioFicheros))]
+namespace RedContactos.WinPhone.Servicios
+{
+    public class ServicioFicheros:IServicioFichero
+    {
+        public async void GuardarTexto(string texto, string fichero)
+        {
+            var carpeta = ApplicationData.Current.LocalFolder;
+            var f = await carpeta.CreateFileAsync(fichero, CreationCollisionOption.ReplaceExisting);
+            using (var stream = new StreamWriter(await f.OpenStreamForWriteAsync()))
+            {
+                stream.Write(texto);
+            }
+        }
+
+        public async Task<String> CargarTexto(String fichero)
+        {
+            try
+            {
+                var carpeta = ApplicationData.Current.LocalFolder;
+                var f = await carpeta.GetItemAsync(fichero);
+                using (var stream = new StreamReader(f.Path))
+                {
+                    var txt = stream.ReadToEnd();
+                    return txt;
+                }
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        public string RecuperarTexto(string fichero)
+        {
+            var data = CargarTexto(fichero);
+            data.Wait();
+            return data.Result;
+        }
+    }
+}
--- RedContactos/RedContactos.iOS/Servicios/ServicioFicheros.cs
+++ RedContactos/RedContactos.iOS/Servicios/ServicioFicheros.cs
@@ -1,7 +1,33 @@
-﻿namespace RedContactos.iOS.Servicios
+﻿using System;
+using System.IO;
+using RedContactos.iOS.Servicios;
+using RedContactos.Service;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ServicioFicheros))]
+namespace RedContactos.iOS.Servicios
 {
-    public class ServicioFicheros
+    public class ServicioFicheros:IServicioFichero
     {
-         
+        public void GuardarTexto(string texto, string fichero)
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var rutafinal = Path.Combine(path, fichero);
+            File.WriteAllText(rutafinal, fichero);
+        }
+
+        public string RecuperarTexto(string fichero)
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var rutafinal = Path.Combine(path, fichero);
+            try
+            {
+                return File.ReadAllText(rutafinal);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
     }
 }
